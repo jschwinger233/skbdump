6a4576fba8e0f1efcfd840ed3c81ae8128cc5a10
gray <gray.liang@isovalent.com>
2023-03-18 17:55:16 +0800

    operate tc filter more robustly

--- config.go
+++ config.go
@@ -12,7 +12,6 @@ import (
 
 type Config struct {
 	Iface         string
-	Priority      uint32
 	PerfOutput    bool
 	SkbFilename   string
 	PcapFilename  string
@@ -26,7 +25,6 @@ var (
 
 func initConfig() {
 	flag.StringVarP(&config.Iface, "interface", "i", "lo", "interface to capture")
-	flag.Uint32VarP(&config.Priority, "priority", "p", 1, "filter priority")
 	flag.BoolVarP(&config.PerfOutput, "perf-output", "", false, "use bpf_perf_event_output to lift payload size limit")
 	flag.StringVarP(&config.SkbFilename, "skb-filename", "s", "skbdump.skb", "output skb filename")
 	flag.StringVarP(&config.PcapFilename, "pcap-filename", "w", "skbdump.pcap", "output pcap filename")
